9995ce81c734e2e9ba2992ae0f5e7bccddb6908a
Jonathan Bermello <bermelloj21@gmail.com>
2026-01-07 19:50:00 -0600

    ojala

--- build.sh
+++ build.sh
@@ -4,5 +4,10 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Crear carpetas para estáticos y fotos con permisos
+mkdir -p staticfiles
+mkdir -p media
+chmod -R 755 media
+
 python manage.py collectstatic --no-input
 python manage.py migrate
