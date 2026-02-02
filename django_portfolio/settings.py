2c86081aa1fb3b696a4557ba352d0604ce73e100
JABS34 <bermelloj21@gmail.com>
2026-02-01 22:05:49 -0600

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -2,10 +2,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-# Ruta base
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# SEGURIDAD
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tu-clave-local')
 DEBUG = 'RENDER' not in os.environ
 
@@ -14,7 +11,6 @@ render_external_hostname = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if render_external_hostname:
     ALLOWED_HOSTS.append(render_external_hostname)
 
-# APLICACIONES
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
@@ -56,7 +52,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "django_portfolio.wsgi.application"
 
-# --- BASE DE DATOS ETERNA (POSTGRESQL) ---
+# CONFIGURACIÓN PARA QUE NO SE BORRE NADA
 DATABASES = {
     'default': dj_database_url.config(
         default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
