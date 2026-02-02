77e82409634792302df4582610d32a5eb807b3ec
JABS34 <bermelloj21@gmail.com>
2026-02-02 00:46:37 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,12 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Esto crea las tablas en la base de datos de Render (PostgreSQL)
+# 1. Aplicar tablas a PostgreSQL
 python manage.py migrate --noinput
 
-# Crea el superusuario jabs6393 (Contraseña: jabs12345)
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', 'jabs12345')"
+# 2. Crear superusuario usando variables de entorno
+# El flag --noinput leerá las variables que configuraremos en el siguiente paso
+python manage.py createsuperuser --noinput || true
 
 mkdir -p media
 chmod -R 755 media
