2a2c6c8e391bf7fc4fbe0fde6c5cb3fe42ba24e5
JABS34 <bermelloj21@gmail.com>
2026-02-01 23:57:09 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ python manage.py migrate
 
 # 4. CREAR SUPERUSUARIO (Todo en una sola línea)
 # Cambia 'TuPasswordAqui' por la que tú quieras
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', 'jonathan100.')"
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', '111.')"
 
 # 5. Permisos de carpetas
 mkdir -p media
