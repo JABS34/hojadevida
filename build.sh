943d6e165bec8bbb4aaab9ed6ce79031bf74f51d
JABS34 <bermelloj21@gmail.com>
2026-02-01 22:06:02 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-# Instalar librerías
 pip install -r requirements.txt
-
-# Archivos estáticos
 python manage.py collectstatic --no-input
-
-# Crear las tablas en la base de datos nueva
 python manage.py migrate
 
-# Asegurar carpeta de imágenes
 mkdir -p media
 chmod -R 755 media
