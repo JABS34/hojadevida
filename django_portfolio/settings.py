368866b578aa37139ef1cce2ca1a2eda98f4b723
JABS34 <bermelloj21@gmail.com>
2026-02-02 00:56:21 -0600

    Update settings.py

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-tu-clave-local')
@@ -49,12 +48,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "django_portfolio.wsgi.application"
 
-# CONFIGURACIÓN POSTGRESQL
+# BASE DE DATOS LOCAL (LA QUE TENÍAS AL PRINCIPIO)
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f'sqlite:///{os.path.join(BASE_DIR, "db.sqlite3")}',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 LANGUAGE_CODE = "es-ec"
