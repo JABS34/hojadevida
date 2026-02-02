0aff688adbba445c4203785338e701d335f663cd
JABS34 <bermelloj21@gmail.com>
2026-02-02 01:36:11 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recolectar archivos estáticos (CSS, JS)
 python manage.py collectstatic --no-input
 
-# Esto creará la tabla que falta en PostgreSQL
-python manage.py migrate --noinput
+# Aplicar migraciones de base de datos
+python manage.py migrate
 
+# CREAR CARPETA DE FOTOS Y DAR PERMISOS
+# Esto evita que salga el cuadro roto por falta de acceso
 mkdir -p media
 chmod -R 755 media
