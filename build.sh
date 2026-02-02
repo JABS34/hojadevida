3e64ebdb3d10e152abd5737fb0a6a86b541d0578
JABS34 <bermelloj21@gmail.com>
2026-02-01 23:53:27 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,15 +2,19 @@
 # exit on error
 set -o errexit
 
-# Instalar dependencias
+# 1. Instalar dependencias
 pip install -r requirements.txt
 
-# Recolectar archivos estáticos
+# 2. Archivos estáticos
 python manage.py collectstatic --no-input
 
-# Aplicar migraciones a PostgreSQL
+# 3. Aplicar tablas a PostgreSQL
 python manage.py migrate
 
-# Asegurar que la carpeta media local tenga permisos
+# 4. CREAR SUPERUSUARIO (Todo en una sola línea)
+# Cambia 'TuPasswordAqui' por la que tú quieras
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', 'jonathan100.')"
+
+# 5. Permisos de carpetas
 mkdir -p media
 chmod -R 755 media
