589cddc902f4183af509376ea9c5377f90bc763b
JABS34 <bermelloj21@gmail.com>
2026-02-02 01:08:32 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,11 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# 1. Instalar dependencias
 pip install -r requirements.txt
-
-# 2. Archivos estáticos
 python manage.py collectstatic --no-input
 
-# 3. CREAR TABLAS FALTANTES (Esto arregla el error 'no such table')
-# Django buscará en tu carpeta 'migrations' y creará lo que falte en db.sqlite3
+# Esto creará la tabla que falta en PostgreSQL
 python manage.py migrate --noinput
 
-# 4. Limpieza de permisos
 mkdir -p media
 chmod -R 755 media
