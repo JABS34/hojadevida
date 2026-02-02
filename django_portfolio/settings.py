61dc8fd0c676b7c9b50bca456a43c9c0b23c0f04
Jonathan Bermello <bermelloj21@gmail.com>
2026-02-02 03:01:31 -0600

    AS

--- django_portfolio/settings.py
+++ django_portfolio/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
 # DEBUG es True en tu PC y False en Render
-DEBUG = "RENDER" not in os.environ
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
