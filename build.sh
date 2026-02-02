a044da266750c7545b3fc84928549040a2f13671
JABS34 <bermelloj21@gmail.com>
2026-02-01 22:19:32 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,19 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# Instalar dependencias
 pip install -r requirements.txt
+
+# Recolectar archivos estáticos
 python manage.py collectstatic --no-input
+
+# GENERAR MIGRACIONES FALTANTES (Esto arregla el error 500)
+python manage.py makemigrations
+
+# APLICAR MIGRACIONES
 python manage.py migrate
 
+# Crear carpeta de fotos si no existe
 mkdir -p media
+chmod -R 755 media
