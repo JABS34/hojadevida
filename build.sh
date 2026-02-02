b65a7f68621b924d2740eebf15fa64556259210a
JABS34 <bermelloj21@gmail.com>
2026-02-02 00:16:35 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,13 +8,6 @@ pip install -r requirements.txt
 # 2. Archivos estáticos
 python manage.py collectstatic --no-input
 
-# 3. Aplicar tablas a PostgreSQL
-python manage.py migrate
-
-# 4. CREAR SUPERUSUARIO (Todo en una sola línea)
-# Cambia 'TuPasswordAqui' por la que tú quieras
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', '111.')"
-
-# 5. Permisos de carpetas
+# 3. Permisos básicos
 mkdir -p media
 chmod -R 755 media
