3ae00e6d956954e4f53483e6459a344eea5ec0df
JABS34 <bermelloj21@gmail.com>
2026-02-02 01:03:52 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
+# 1. Instalar dependencias
 pip install -r requirements.txt
+
+# 2. Archivos estáticos
 python manage.py collectstatic --no-input
 
-# No ponemos migrate ni createsuperuser para que use lo que ya está en tu db.sqlite3
+# 3. CREAR TABLAS FALTANTES (Esto arregla el error 'no such table')
+# Django buscará en tu carpeta 'migrations' y creará lo que falte en db.sqlite3
+python manage.py migrate --noinput
+
+# 4. Limpieza de permisos
 mkdir -p media
 chmod -R 755 media
