80a19e6f06d628e781a784f1ce3f63dfec100cbf
JABS34 <bermelloj21@gmail.com>
2026-02-02 00:43:04 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,11 +4,11 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 
-# Esto creará la tabla 'tasks_configuracionvisible' en PostgreSQL
-python manage.py migrate
+# Esto crea las tablas en la base de datos de Render (PostgreSQL)
+python manage.py migrate --noinput
 
-# Crea tu usuario (Usa una contraseña que no tenga puntos al final, ej: jabs123)
-python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', '', '1111')"
+# Crea el superusuario jabs6393 (Contraseña: jabs12345)
+python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='jabs6393').exists() or User.objects.create_superuser('jabs6393', 'admin@ejemplo.com', 'jabs12345')"
 
 mkdir -p media
 chmod -R 755 media
