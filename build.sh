dcae21f8514bf87143826a9c0fdf994b4708df3c
JABS34 <bermelloj21@gmail.com>
2026-02-02 00:21:01 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ pip install -r requirements.txt
 # 2. Archivos estáticos
 python manage.py collectstatic --no-input
 
-# 3. Permisos básicos
+# 3. CREAR LAS TABLAS QUE FALTAN (Soluciona el error de la imagen)
+python manage.py migrate
+
+# 4. Permisos básicos
 mkdir -p media
 chmod -R 755 media
