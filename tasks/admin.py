74ca1f758e9332f3dd88e977a79781f35b464da4
JABS34 <bermelloj21@gmail.com>
2026-02-03 13:07:51 -0500

    Update admin.py

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,5 +1,4 @@
 from django.contrib import admin
-from django.utils.html import format_html
 from .models import (
     Task, DatosPersonales, ExperienciaLaboral, 
     CursosRealizados, Reconocimiento, VentaGarage,
@@ -7,7 +6,7 @@ from .models import (
     LenguajeProgramacion, Habilidad, ConfiguracionVisible
 )
 
-# --- PERSONALIZACIÓN DEL ENCABEZADO DEL PANEL ---
+# --- CONFIGURACIÓN VISUAL DEL PANEL ---
 admin.site.site_header = "Administración de Portafolio"
 admin.site.site_title = "Panel de Control"
 admin.site.index_title = "Gestión de Contenido Web"
@@ -22,117 +21,68 @@ class TaskAdmin(admin.ModelAdmin):
 # 2. DATOS PERSONALES
 @admin.register(DatosPersonales)
 class DatosPersonalesAdmin(admin.ModelAdmin):
-    list_display = ('nombres', 'apellidos', 'numerocedula', 'estado_visual')
+    # CORREGIDO: 'activarparaqueseveaenfront' está en display Y en editable
+    list_display = ('nombres', 'apellidos', 'numerocedula', 'activarparaqueseveaenfront')
     list_editable = ('activarparaqueseveaenfront',)
     search_fields = ('nombres', 'apellidos', 'numerocedula')
-    
-    # Truco para mostrar un icono en vez de True/False
-    def estado_visual(self, obj):
-        return obj.activarparaqueseveaenfront
-    estado_visual.boolean = True
-    estado_visual.short_description = "Visible en Web"
 
 # 3. EXPERIENCIA LABORAL
 @admin.register(ExperienciaLaboral)
 class ExperienciaLaboralAdmin(admin.ModelAdmin):
-    list_display = ('cargodesempenado', 'nombrempresa', 'fechainiciogestion', 'visible')
+    list_display = ('cargodesempenado', 'nombrempresa', 'fechainiciogestion', 'activarparaqueseveaenfront')
     list_filter = ('activarparaqueseveaenfront', 'nombrempresa')
     search_fields = ('cargodesempenado', 'nombrempresa')
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 4. RECONOCIMIENTOS
 @admin.register(Reconocimiento)
 class ReconocimientoAdmin(admin.ModelAdmin):
-    list_display = ('descripcionreconocimiento', 'tiporeconocimiento', 'entidadpatrocinadora', 'visible')
+    list_display = ('descripcionreconocimiento', 'tiporeconocimiento', 'entidadpatrocinadora', 'activarparaqueseveaenfront')
     list_filter = ('tiporeconocimiento', 'activarparaqueseveaenfront')
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 5. CURSOS REALIZADOS
 @admin.register(CursosRealizados)
 class CursosRealizadosAdmin(admin.ModelAdmin):
-    list_display = ('nombrerecurso', 'entidadpatrocinadora', 'totalhoras', 'visible')
+    list_display = ('nombrerecurso', 'entidadpatrocinadora', 'totalhoras', 'activarparaqueseveaenfront')
     search_fields = ('nombrerecurso', 'entidadpatrocinadora')
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 6. PRODUCTOS ACADÉMICOS
 @admin.register(ProductosAcademicos)
 class ProductosAcademicosAdmin(admin.ModelAdmin):
-    list_display = ('nombrerecurso', 'clasificador', 'visible')
+    list_display = ('nombrerecurso', 'clasificador', 'activarparaqueseveaenfront')
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 7. PRODUCTOS LABORALES
 @admin.register(ProductosLaborales)
 class ProductosLaboralesAdmin(admin.ModelAdmin):
-    list_display = ('nombreproducto', 'fechaproducto', 'visible')
+    list_display = ('nombreproducto', 'fechaproducto', 'activarparaqueseveaenfront')
     list_filter = ('fechaproducto',)
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 8. VENTA GARAGE
 @admin.register(VentaGarage)
 class VentaGarageAdmin(admin.ModelAdmin):
-    list_display = ('nombreproducto', 'precio_formato', 'estadoproducto', 'visible')
+    list_display = ('nombreproducto', 'valordelbien', 'estadoproducto', 'activarparaqueseveaenfront')
     list_filter = ('estadoproducto', 'activarparaqueseveaenfront')
-    list_editable = ('activarparaqueseveaenfront', 'estadoproducto')
+    list_editable = ('valordelbien', 'estadoproducto', 'activarparaqueseveaenfront')
     search_fields = ('nombreproducto',)
-    
-    def precio_formato(self, obj):
-        return f"${obj.valordelbien}"
-    precio_formato.short_description = "Precio"
-
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 9. LENGUAJES DE PROGRAMACIÓN
 @admin.register(LenguajeProgramacion)
 class LenguajeProgramacionAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'barra_progreso', 'visible')
+    # CORREGIDO: 'nivel_porcentaje' debe estar aquí para poder editarlo
+    list_display = ('nombre', 'nivel_porcentaje', 'activarparaqueseveaenfront')
     list_editable = ('nivel_porcentaje', 'activarparaqueseveaenfront')
-    
-    # Muestra una barrita visual en el admin
-    def barra_progreso(self, obj):
-        return format_html(
-            '<div style="width:100px; background:#ddd; border-radius:5px;">'
-            '<div style="width:{}%; background:#28a745; height:10px; border-radius:5px;"></div>'
-            '</div>',
-            obj.nivel_porcentaje
-        )
-    barra_progreso.short_description = "Nivel"
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
+    list_filter = ('activarparaqueseveaenfront',)
 
 # 10. HABILIDADES
 @admin.register(Habilidad)
 class HabilidadAdmin(admin.ModelAdmin):
-    list_display = ('nombre_habilidad', 'categoria', 'visible')
+    list_display = ('nombre_habilidad', 'categoria', 'activarparaqueseveaenfront')
     list_filter = ('categoria', 'activarparaqueseveaenfront')
     list_editable = ('activarparaqueseveaenfront',)
-    
-    def visible(self, obj):
-        return obj.activarparaqueseveaenfront
-    visible.boolean = True
 
 # 11. CONFIGURACIÓN DE VISIBILIDAD
 @admin.register(ConfiguracionVisible)
